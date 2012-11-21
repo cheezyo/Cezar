b051bb06be0522722f55de17e6c6da346d1bbd95
Cezar Sincan <cezarsincan@gmail.com>
2012-11-21 11:35:29 +0100

    df

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cezar::Application.config.secret_token = 'ce292edbf019bf0540f50c7153d40e8cad26cdd793b66cc08df6d201c782e3ce7c46035d56ad141eb533d8633d05a501c1e88d0e23be56de2393e8e059d4c755'
